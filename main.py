23413601869ff7343fcedde79ea3286a152f69d7
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2025-06-22 12:40:26 +0530

    notifies alerts for new job postings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import resend
 from slowapi import Limiter
 from datetime import datetime
-from configs import GRAPHQL_URL, REDIS_URL
+from configs import GRAPHQL_URL, REDIS_URL, RESEND_APIKEY, EMAILS_TO_NOTIFY
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 import requests, logging, aioredis, json, pytz
@@ -12,7 +13,6 @@ from utils import rate_limit_exceeded_handler, QUERY, HEADERS
 
 # SETUP LOGGER
 logger = logging.getLogger(__file__)
-
 
 
 
@@ -29,6 +29,7 @@ app = FastAPI(
 
 # INIT
 ist = pytz.timezone("Asia/Kolkata")
+resend.api_key = RESEND_APIKEY
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
@@ -66,6 +67,28 @@ async def check_alive(request: Request):
 
 
 
+def notify_via_email(new_jobs):
+    if new_jobs:
+        job_html_parts = []
+        for idx, job in enumerate(new_jobs, start=1):
+            job_data = job.get("node", {}).get("job", {})
+
+            title = job_data.get("title", "N/A")
+            job_html_parts.append(
+                f"<p><strong>{idx}. {title}</strong><br>"
+            )
+
+        job_html_content = "<h3>NYU New OnCampus Job(s) on Handshake:</h3>" + "".join(job_html_parts)
+
+        params: resend.Emails.SendParams = {
+            "from": "alerts@resend.dev",
+            "to": EMAILS_TO_NOTIFY.split(';'),
+            "subject": "NYU Handshake New OnCampus Job(s) Alert!",
+            "html": job_html_content,
+        }
+        return params
+
+
 @app.post("/search-jobs")
 async def search_jobs():
     new_jobs = []
@@ -96,12 +119,17 @@ async def search_jobs():
         res = requests.post(GRAPHQL_URL, json=payload, headers=HEADERS)
         jobs = res.json().get("data").get("jobSearch").get("edges")
         await app.state.redis.set("LAST_JOB_POSTING_CHECK", json.dumps({'last_check': datetime.now(ist).replace(microsecond=0).isoformat()}))
-        await app.state.redis.expire("LAST_JOB_POSTING_CHECK", 24 * 60 * 60 * 30)
-
+        await app.state.redis.expire("LAST_JOB_POSTING_CHECK", 24 * 60 * 60 * 50)
         if last_check_timing:
             for job in jobs:
                 if datetime.fromisoformat(job.get("node").get("job").get("createdAt")) > datetime.fromisoformat(last_check_timing):
                     new_jobs.append(job)
-        return new_jobs
+        if new_jobs:
+            params = notify_via_email(new_jobs)
+            resend.Emails.send(params)
+            logger.info("Alert Send")
+            return {'message': 'emails notified for new jobs'}
+        else:
+            return {'message': 'no new jobs'}
     except requests.RequestException as e:
         raise HTTPException(status_code=500, detail=f"Request failed: {str(e)}")
